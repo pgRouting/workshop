944b8b529dee9883a4d4aab7ddd310d230a7b216
cvvergara <vicky_vergara@hotmail.com>
2016-07-27 13:46:39 -0500

    trying bootstrap 2

--- docs/conf.py
+++ docs/conf.py
@@ -146,17 +146,17 @@ html_short_title = "Workshop - FOSS4G Bonn"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/pgrouting.png"
+html_logo = "static/pgrouting.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "_static/favicon.ico"
+html_favicon = "static/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
