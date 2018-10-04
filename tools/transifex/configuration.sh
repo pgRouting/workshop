45ea228bdc0eefe5763c88ca6dcb3acaa44fc442
cvvergara <vicky@georepublic.de>
2018-10-04 12:58:09 -0500

    adding some configuration files

---
+++ tools/transifex/configuration.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ------------------------------------------------------------------------------
+# pgRouting Scripts
+# Copyright(c) pgRouting Contributors
+#
+# Initially create POT files (probably not necessary)
+# ------------------------------------------------------------------------------
+
+ROOT=$(pwd)
+CONFIG="."
+DOCDIR="locale"
+LANGUAGES=(de es ja fr)
+
