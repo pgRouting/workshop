447df0e62e16ffcf50cd09a126ffb10c8332feb2
cvvergara <vicky@georepublic.de>
2020-05-04 20:16:14 -0500

    up to install pgrouting

--- scripts/create-db.sh
+++ scripts/create-db.sh
@@ -1,5 +1,32 @@
 set -e
 rm -f database_created.txt
-dropdb --if-exists __workshop__
-createdb __workshop__
+dropdb --if-exists city_routing
+
+# 4.1.1 from-here
+
+# Create the database
+createdb city_routing
+
+# login as user "user"
+psql city_routing << EOF
+
+-- add PostGIS functions
+CREATE EXTENSION postgis;
+
+-- add pgRouting functions
+CREATE EXTENSION pgrouting;
+
+-- Inspect the pgRouting installation
+\dx+ pgrouting
+
+-- View pgRouting version
+SELECT pgr_version();
+
+-- exit psql
+\q
+
+EOF
+
+# 4.1.1 to-here
+
 touch database_created.txt
