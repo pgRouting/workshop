5c7e9d786d15217060d5f051f81835a6fd6a8777
cvvergara <vicky@georepublic.de>
2020-08-25 19:52:43 -0500

    making things work

--- docs/scripts/configuration/osmid_configuration.sql
+++ docs/scripts/configuration/osmid_configuration.sql
@@ -1,6 +1,21 @@
 \t
 \o osmid_configuration.conf
-WITH a AS (SELECT row_number() OVER(), osm_id, id FROM ways_vertices_pgr
-WHERE osm_id IN (123392877, 255093299, 1886700005, 6159253045, 6498351588)
-)
-SELECT 'ID_' || row_number, id from a;
+WITH a AS (SELECT 1 AS r, osm_id, id FROM ways_vertices_pgr
+WHERE osm_id = @OSMID_1@)
+SELECT 'ID_' || r, id from a;
+
+WITH a AS (SELECT 2 AS r, osm_id, id FROM ways_vertices_pgr
+WHERE osm_id = @OSMID_2@)
+SELECT 'ID_' || r, id from a;
+
+WITH a AS (SELECT 3 AS r, osm_id, id FROM ways_vertices_pgr
+WHERE osm_id = @OSMID_3@)
+SELECT 'ID_' || r, id from a;
+
+WITH a AS (SELECT 4 AS r, osm_id, id FROM ways_vertices_pgr
+WHERE osm_id = @OSMID_4@)
+SELECT 'ID_' || r, id from a;
+
+WITH a AS (SELECT 5 AS r, osm_id, id FROM ways_vertices_pgr
+WHERE osm_id = @OSMID_5@)
+SELECT 'ID_' || r, id from a;
