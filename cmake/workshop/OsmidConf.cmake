96897ade0b43426f7957d2566a397599a34e7149
cvvergara <vicky@georepublic.de>
2020-08-25 19:04:16 -0500

    creating a cmake map variable parser

---
+++ cmake/workshop/OsmidConf.cmake
@@ -0,0 +1,31 @@
+#---------------------------------------------
+#  OSM identifiers configuration
+#---------------------------------------------
+#
+# Gets the corresponding id from the osm_id data
+#----------------------
+
+#
+# file:
+# build/docs/scripts/configuration/osmid_configuration.conf
+# must exists
+
+MACRO(INSERT_INTO_MAP_NAME _NAME _VALUE)
+    SET("${_NAME}" "${_VALUE}")
+ENDMACRO(INSERT_INTO_MAP_NAME)
+
+message(STATUS "PROJECT_BINARY_DIR ${PROJECT_BINARY_DIR}")
+# Get the configuration strings
+file(STRINGS "${PROJECT_BINARY_DIR}/docs/scripts/configuration/osmid_configuration.conf" CONFIGURATION_STRINGS)
+
+
+foreach(line ${CONFIGURATION_STRINGS})
+    string(REGEX REPLACE "^(.*)\\|(.*)" "\\1" id_name ${line})
+    string(REGEX REPLACE "^(.*)\\|(.*)" "\\2" id_value ${line})
+
+    ## remove leading & trailing spaces
+    string(STRIP ${id_name} id_name)
+    string(STRIP ${id_value} id_value)
+
+    INSERT_INTO_MAP_NAME("${id_name}" "${id_value}")
+endforeach()
