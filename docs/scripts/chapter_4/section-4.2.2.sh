322b1ca3410c2673af8614dccd07598641e8ddd9
omshinde <rajatshinde2303@gmail.com>
2020-11-07 22:34:42 +0530

    Update OSGeo BA_AR osm data link

--- docs/scripts/chapter_4/section-4.2.2.sh
+++ docs/scripts/chapter_4/section-4.2.2.sh
@@ -4,7 +4,7 @@ set -e
 # 4.2.2 from-here
 CITY="@PGR_WORKSHOP_CITY_FILE@"
 wget -N --progress=dot:mega \
-"http://download.osgeo.org/livedvd/data/osm/$CITY/$CITY.osm.bz2"
+"http://download.osgeo.org/livedvd/14/osm/$CITY.osm.bz2"
 bunzip2 -f "$CITY.osm.bz2"
 # 4.2.2 to-here
 
