ebe8ef84d205eecb3a645fbd30740f22f04ca8eb
Swapnil Joshi <203310005@iitb.ac.in>
2021-07-28 22:02:37 +0530

    Adding more queries for calculating population

--- docs/scripts/un_sdg/all_exercises_sdg1.sql
+++ docs/scripts/un_sdg/all_exercises_sdg1.sql
@@ -20,3 +20,69 @@ SELECT count(*) FROM roads_ways_vertices_pgr;
 
 -- Counting the number of buildings 
 SELECT count(*) FROM buildings_ways;
+
+-- Showing the structure of the table
+\dS+ buildings_ways
+
+-- Add a spatial column to the table
+SELECT AddGeometryColumn ('buildings','buildings_ways','poly_geom',4326,'POLYGON',2);
+
+-- Removing the geometries that are not polygons 
+DELETE FROM buildings_ways WHERE ST_NumPoints(the_geom) < 4 OR ST_IsClosed(the_geom) = false;
+
+-- Creating the polygons
+UPDATE buildings_ways SET poly_geom = ST_MakePolygon(the_geom);
+
+ALTER TABLE buildings_ways
+ADD COLUMN area INTEGER;
+
+
+UPDATE buildings_ways SET area = ST_Area(poly_geom::geography)::INTEGER;
+
+
+CREATE OR REPLACE FUNCTION  population(tag_id INTEGER,area INTEGER)
+RETURNS INTEGER AS 
+$BODY$
+DECLARE 
+population INTEGER;
+BEGIN 
+  IF tag_id < 120 THEN population = area * 1;
+  ELSIF tag_id < 130 THEN population = area * 2;
+  ELSIF tag_id < 150 THEN population = area * 0.8;
+  ELSE population = area * 3;
+  END IF;
+  RETURN population;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+\o
