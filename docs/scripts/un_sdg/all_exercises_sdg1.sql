97393ead1b3a7d6690a4574cc15ba21ab77bbfd8
Swapnil Joshi <203310005@iitb.ac.in>
2021-07-28 20:48:06 +0530

    Current Result

--- docs/scripts/un_sdg/all_exercises_sdg1.sql
+++ docs/scripts/un_sdg/all_exercises_sdg1.sql
@@ -1,8 +1,22 @@
+\o all_exercises_sdg1.txt
+-- Enumerate all the schemas
+\dn
+
+-- Show the current search path
+SHOW search_path;
+
+-- Set the search path
+SET search_path TO roads,buildings,public;
+SHOW search_path;
+
 -- Enumerate all the tables
 \dt
 
--- Counting the number of Edges
-SELECT count(*) FROM ways;
+-- Counting the number of Edges of roads
+SELECT count(*) FROM roads_ways;
 
--- Counting the number of Vertices
-SELECT count(*) FROM ways_vertices_pgr;
+-- Counting the number of Vertices of roads
+SELECT count(*) FROM roads_ways_vertices_pgr;
+
+-- Counting the number of buildings 
+SELECT count(*) FROM buildings_ways;
