c265512e0125503cc5ac8414b3d11fabbe521868
Swapnil Joshi <203310005@iitb.ac.in>
2021-09-08 10:38:29 +0530

    Adding SDG7 scripts

---
+++ docs/scripts/un_sdg/sdg7/all_exercises_sdg7.sql
@@ -0,0 +1,87 @@
+\o setting_search_path.txt
+-- Enumerate all the schemas
+\dn
+
+
+-- Show the current search path
+SHOW search_path;
+
+-- Set the search path
+SET search_path TO roads,buildings,public;
+SHOW search_path;
+
+
+-- Enumerate all the tables
+\dt
+
+
+
+\o count_roads.txt
+-- Counting the number of Edges of roads
+SELECT count(*) FROM roads_ways;
+
+-- Counting the number of Vertices of roads
+SELECT count(*) FROM roads_ways_vertices_pgr;
+
+-- UN SDG7: Affordable and Clean Energy 
+
+\o discard_disconnected_roads.txt
+
+-- Process to discard disconnected roads
+
+-- Add a column for storing the component
+ALTER TABLE roads_ways_vertices_pgr
+ADD COLUMN component INTEGER;
+
+-- Update the vertices with the component number
+UPDATE roads_ways_vertices_pgr set component = subquery.component
+FROM (SELECT * FROM pgr_connectedComponents('SELECT gid AS id, source, target, cost, reverse_cost FROM roads_ways')) AS subquery
+WHERE id = node;
+
+-- These components are to be removed
+WITH
+subquery AS (SELECT component, count(*) FROM roads_ways_vertices_pgr GROUP BY component)
+SELECT component FROM subquery where count != (SELECT max(count) FROM subquery);
+
+-- The edges that need to be removed
+WITH
+subquery AS (SELECT component, count(*) FROM roads_ways_vertices_pgr GROUP BY component),
+to_remove AS (SELECT component FROM subquery where count != (SELECT max(count) FROM subquery))
+SELECT id FROM roads_ways_vertices_pgr WHERE component IN (SELECT * FROM to_remove);
+
+-- Removing the unwanted edges
+DELETE FROM roads_ways WHERE source IN (
+WITH
+subquery AS (SELECT component, count(*) FROM roads_ways_vertices_pgr GROUP BY component),
+to_remove AS (SELECT component FROM subquery where count != (SELECT max(count) FROM subquery))
+SELECT id FROM roads_ways_vertices_pgr WHERE component IN (SELECT * FROM to_remove)
+);
+
+-- Deleting unused vertices
+WITH
+subquery AS (SELECT component, count(*) FROM roads_ways_vertices_pgr GROUP BY component),
+to_remove AS (SELECT component FROM subquery where count != (SELECT max(count) FROM subquery))
+DELETE FROM roads_ways_vertices_pgr WHERE component IN (SELECT * FROM to_remove);
+
+\o kruskal_minimum_spanning_tree.txt
+
+ALTER TABLE roads_ways
+ADD COLUMN id INTEGER;
+
+UPDATE roads_ways SET id = gid;
+
+SELECT source,target,edge, r.the_geom 
+FROM pgr_kruskalDFS(
+    'SELECT id, source, target, cost, reverse_cost, the_geom FROM roads.roads_ways ORDER BY id',
+    91), roads.roads_ways as r
+where edge = r.gid;
+
+
+
+
+
+
+
+
+
+\o
---
+++ docs/scripts/un_sdg/sdg7/run.sh
@@ -0,0 +1,5 @@
+./create_mumbai_sdg7.sh 
+./import_mumbai_sdg7_roads.sh
+./import_mumbai_sdg7_buildings.sh
+
+psql mumbai
