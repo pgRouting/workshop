f15f9f15c09dcf950c9facf9958a1164ebda46b1
Swapnil Joshi <203310005@iitb.ac.in>
2021-09-11 16:37:35 +0530

    Fixing spaces in codeblocks

--- docs/scripts/un_sdg/sdg11/all_exercises_sdg11.sql
+++ docs/scripts/un_sdg/sdg11/all_exercises_sdg11.sql
@@ -13,17 +13,13 @@ SHOW search_path;
 
 -- Enumerate all the tables
 \dt
-
 \o count_waterways.txt
 -- Counting the number of Edges of waterways
 SELECT * FROM waterways_ways limit 2; 
 
 -- Counting the number of Vertices of waterways
 SELECT count(*) FROM waterways_ways_vertices_pgr;
-
-
 \o connected_components.txt
-
 -- Add a column for storing the component
 ALTER TABLE waterways_ways_vertices_pgr
 ADD COLUMN component INTEGER;
@@ -32,7 +28,9 @@ ALTER TABLE waterways_ways
 ADD COLUMN component INTEGER;
 
 -- remove rivers on the swamp area (we want the rivers which are only on land)
-DELETE FROM waterways_ways WHERE osm_id IN (721133202, 908102930, 749173392, 652172284, 126774195, 720395312);
+DELETE FROM waterways_ways 
+WHERE osm_id 
+IN (721133202, 908102930, 749173392, 652172284, 126774195, 720395312);
 
 -- Update the vertices with the component number
 UPDATE waterways_ways_vertices_pgr SET component = subquery.component
@@ -50,10 +48,7 @@ WHERE id = source;
 CREATE TABLE city_vertex (id BIGINT, name TEXT, geom geometry);
 INSERT INTO city_vertex(id, name, geom) VALUES (
 5,'Munshigang', ST_SetSRID(ST_Point(89.1967,22.2675),4326));
-
-
 \o creating_buffers_city.txt
-
 -- Creating buffers for city
 -- Adding column to store Buffer geometry
 ALTER TABLE waterways.city_vertex
@@ -66,8 +61,6 @@ WHERE  name = 'Munshigang';
 
 -- Showing results of Buffer operation
 SELECT city_buffer FROM waterways.city_vertex;
-
-
 -- Creating a function that gets the city_buffer
 CREATE OR REPLACE FUNCTION get_city_buffer(city_id INTEGER)
 RETURNS geometry AS
@@ -75,14 +68,12 @@ $BODY$
 SELECT city_buffer FROM city_vertex WHERE id = city_id;
 $BODY$
 LANGUAGE SQL;
-
 \o Intersecting_components.txt
 
 -- Intersection of City Buffer and River Components
 SELECT DISTINCT component
 FROM waterways.city_vertex, waterways.waterways_ways
 WHERE ST_Intersects(the_geom, get_city_buffer(5));
-
 \o creating_rain_zones_buffers_waterways.txt
 
 
@@ -98,12 +89,8 @@ WHERE ST_Intersects(the_geom, get_city_buffer(5));
 
 -- Showing the zone, where if it rains,the city would be affected
 SELECT rain_zone FROM waterways.waterways_ways;
-
-
--- Combining mutliple rain zones.
+-- Combining mutliple rain zones
 SELECT ST_Union(rain_zone) AS Combined_Rain_Zone
 FROM waterways_ways;
-
-
 \o
 
