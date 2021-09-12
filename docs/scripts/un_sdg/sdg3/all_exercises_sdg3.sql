29ee7f456f2000fe07fe4aaca2dd0f6131772a7d
Swapnil Joshi <203310005@iitb.ac.in>
2021-09-12 15:17:49 +0530

    Rearranging SDG3 images

--- docs/scripts/un_sdg/sdg3/all_exercises_sdg3.sql
+++ docs/scripts/un_sdg/sdg3/all_exercises_sdg3.sql
@@ -104,36 +104,70 @@ ALTER TABLE roads_ways_vertices_pgr
 ADD COLUMN component INTEGER;
 
 -- Update the vertices with the component number
-UPDATE roads_ways_vertices_pgr set component = subquery.component
-FROM (SELECT * FROM pgr_connectedComponents('SELECT gid AS id, source, target, cost, reverse_cost FROM roads_ways')) AS subquery
+UPDATE roads_ways_vertices_pgr 
+SET component = subquery.component 
+FROM (
+	SELECT * FROM pgr_connectedComponents(
+		'SELECT gid AS id, source, target, cost, reverse_cost 
+		FROM roads_ways'
+			)
+		) 
+AS subquery
 WHERE id = node;
 
 -- These components are to be removed
 WITH
-subquery AS (SELECT component, count(*) FROM roads_ways_vertices_pgr GROUP BY component)
-SELECT component FROM subquery where count != (SELECT max(count) FROM subquery);
+subquery AS (
+	SELECT component, count(*) 
+	FROM roads_ways_vertices_pgr 
+	GROUP BY component
+	)
+SELECT component FROM subquery 
+WHERE count != (SELECT max(count) FROM subquery);
 
 -- The edges that need to be removed
 WITH
-subquery AS (SELECT component, count(*) FROM roads_ways_vertices_pgr GROUP BY component),
-to_remove AS (SELECT component FROM subquery where count != (SELECT max(count) FROM subquery))
-SELECT id FROM roads_ways_vertices_pgr WHERE component IN (SELECT * FROM to_remove);
+subquery AS (
+	SELECT component, count(*) 
+	FROM roads_ways_vertices_pgr 
+	GROUP BY component
+	),
+to_remove AS (
+	SELECT component FROM subquery 
+	WHERE count != (SELECT max(count) FROM subquery)
+	)
+SELECT id FROM roads_ways_vertices_pgr 
+WHERE component IN (SELECT * FROM to_remove);
 
 -- Removing the unwanted edges
 DELETE FROM roads_ways WHERE source IN (
-WITH
-subquery AS (SELECT component, count(*) FROM roads_ways_vertices_pgr GROUP BY component),
-to_remove AS (SELECT component FROM subquery where count != (SELECT max(count) FROM subquery))
-SELECT id FROM roads_ways_vertices_pgr WHERE component IN (SELECT * FROM to_remove)
+	WITH
+	subquery AS (
+		SELECT component, count(*) 
+		FROM roads_ways_vertices_pgr 
+		GROUP BY component
+		),
+to_remove AS (
+		SELECT component FROM subquery 
+		WHERE count != (SELECT max(count) FROM subquery)
+		)
+	SELECT id FROM roads_ways_vertices_pgr 
+	WHERE component IN (SELECT * FROM to_remove)
 );
 
 -- Deleting unused vertices
 WITH
-subquery AS (SELECT component, count(*) FROM roads_ways_vertices_pgr GROUP BY component),
-to_remove AS (SELECT component FROM subquery where count != (SELECT max(count) FROM subquery))
-DELETE FROM roads_ways_vertices_pgr WHERE component IN (SELECT * FROM to_remove);
-
-
+subquery AS (
+	SELECT component, count(*) 
+	FROM roads_ways_vertices_pgr 
+	GROUP BY component
+	),
+to_remove AS (
+	SELECT com ponent FROM subquery 
+	WHERE count != (SELECT max(count) FROM subquery)
+	)
+DELETE FROM roads_ways_vertices_pgr 
+WHERE component IN (SELECT * FROM to_remove);
 
 \o population_residing_along_the_road.txt
 
@@ -165,7 +199,7 @@ ALTER TABLE roads_ways
 ADD COLUMN population INTEGER;
 
 -- Update the roads with the sum of population of buildings nearest to it
-UPDATE roads_ways set population = sum
+UPDATE roads_ways SET population = sum
 FROM (SELECT edge_id, sum(population) FROM buildings_ways GROUP BY edge_id) AS subquery
 WHERE gid = edge_id;                                                                                                              
                
@@ -174,12 +208,70 @@ SELECT population FROM roads_ways WHERE gid = 441;
 
 
 -- road_population_to_here
+/*
+-- SOLVING THE PROBLEM 
+
+-- Use pgr_DrivingDistance to get the vertices that go to the different locations.
+
+
+-- Finding the Centroids of polygons
+
+SELECT ST_AsText(ST_Centroid(buildings_ways.poly_geom));
+
+
+-- Function for calculating Service area of each hospital
+CREATE OR REPLACE FUNCTION  service_area(tag_id INTEGER)
+RETURNS INTEGER AS 
+$BODY$
+DECLARE 
+service_area GEOMETRY;
+BEGIN 
+  IF tag_id <= 318 THEN 
+  CREATE TABLE distances AS (SELECT a.node AS id, a.agg_cost AS distance, b.the_geom
+    FROM pgr_drivingDistance(
+        SELECT ( AS id, source, target, population as cost, population as reverse_cost FROM roads_ways,
+        Node1,
+        Node2,
+        true
+    ) a, ways_vertices_pgr b WHERE a.node = b.id
+	);
+  END IF;
+  RETURN service_area;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+
+-- Creatating a Table to store Service area of each hospital
+DROP TABLE IF EXISTS distances;
+CREATE TABLE distances (
+    gid        INTEGER,
+    service_area     GEOMETRY,
+);
+
+-- Calculating and storing Service area of each hospital
+UPDATE distances 
+SET service_area = service_area(tag_id)::GEOMETRY; 
+
+-- Finding the areas which are not covered by current hospitals
+
+	-- 1. Finding Polygons of the service area of the current hospitals
+	-- 2. Finding intersection of the buildings and polygons with the service area
+
+
+-- Proposing new locations for mobile hospitals
+
+	-- 1. Checking availability of schools/universities/hotels/grounds for temporary hospital locations
+	-- 2. If present, assigning the optimal locations to them.
+	-- 3. Check the service area once again. If the service area cover the whole study area, Task is completed.
+	
+	-- Questions: 
+		-- i. What if no schools/universities/hotels/grounds are available?
+		-- ii. What is the service area of new polygon doesnot satisfy the needs of complete area?
 
 
 
-
-
-
+*/
 
 
 
