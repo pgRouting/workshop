849c61b443d7bad4e8662b97a1b3c13cf4b86cb9
Swapnil Joshi <203310005@iitb.ac.in>
2021-09-12 19:37:43 +0530

    Adding scripts for Chapter 3 SDG 3

--- docs/scripts/un_sdg/sdg3/all_exercises_sdg3.sql
+++ docs/scripts/un_sdg/sdg3/all_exercises_sdg3.sql
@@ -58,9 +58,6 @@ ADD COLUMN area INTEGER;
 UPDATE buildings_ways 
 SET area = ST_Area(poly_geom::geography)::INTEGER;
 
-
-
-
 -- UN SDG3: Good Health and Well Being
 
 -- population_function_from_here
@@ -163,7 +160,7 @@ subquery AS (
 	GROUP BY component
 	),
 to_remove AS (
-	SELECT com ponent FROM subquery 
+	SELECT component FROM subquery 
 	WHERE count != (SELECT max(count) FROM subquery)
 	)
 DELETE FROM roads_ways_vertices_pgr 
@@ -175,7 +172,7 @@ WHERE component IN (SELECT * FROM to_remove);
 
 -- nearest_road_from_here
 
--- Create Function for finding the nearest edge
+-- Create Function for finding the closest edge
 CREATE OR REPLACE FUNCTION closest_edge(geom GEOMETRY)
 RETURNS BIGINT AS
 $BODY$
@@ -183,11 +180,11 @@ SELECT gid FROM roads_ways ORDER BY geom <-> the_geom LIMIT 1;
 $BODY$
 LANGUAGE SQL;
 
--- Add a column for storing the nearest edge
+-- Add a column for storing the closest edge
 ALTER TABLE buildings_ways
 ADD COLUMN edge_id INTEGER;
 
--- Store the edge_id of the nearest edge in the column
+-- Store the edge_id of the closest edge in the column
 UPDATE buildings_ways SET edge_id = closest_edge(poly_geom);
 
 -- nearest_road_to_here
@@ -198,92 +195,71 @@ UPDATE buildings_ways SET edge_id = closest_edge(poly_geom);
 ALTER TABLE roads_ways
 ADD COLUMN population INTEGER;
 
--- Update the roads with the sum of population of buildings nearest to it
+-- Update the roads with the sum of population of buildings closest to it
 UPDATE roads_ways SET population = sum
 FROM (SELECT edge_id, sum(population) FROM buildings_ways GROUP BY edge_id) AS subquery
 WHERE gid = edge_id;                                                                                                              
                
 -- testing
 SELECT population FROM roads_ways WHERE gid = 441;
-
-
 -- road_population_to_here
-/*
--- SOLVING THE PROBLEM 
 
--- Use pgr_DrivingDistance to get the vertices that go to the different locations.
-
-
--- Finding the Centroids of polygons
-
-SELECT ST_AsText(ST_Centroid(buildings_ways.poly_geom));
-
-
--- Function for calculating Service area of each hospital
-CREATE OR REPLACE FUNCTION  service_area(tag_id INTEGER)
-RETURNS INTEGER AS 
+\o nearest_vertex.txt
+-- finding the closest road vertex
+CREATE OR REPLACE FUNCTION closest_vertex(geom GEOMETRY)
+RETURNS BIGINT AS
 $BODY$
-DECLARE 
-service_area GEOMETRY;
-BEGIN 
-  IF tag_id <= 318 THEN 
-  CREATE TABLE distances AS (SELECT a.node AS id, a.agg_cost AS distance, b.the_geom
-    FROM pgr_drivingDistance(
-        SELECT ( AS id, source, target, population as cost, population as reverse_cost FROM roads_ways,
-        Node1,
-        Node2,
-        true
-    ) a, ways_vertices_pgr b WHERE a.node = b.id
-	);
-  END IF;
-  RETURN service_area;
-END;
+SELECT id FROM roads_ways_vertices_pgr ORDER BY geom <-> the_geom LIMIT 1;
 $BODY$
-LANGUAGE plpgsql;
+LANGUAGE SQL;
 
+ALTER TABLE roads_ways
+ADD COLUMN id INTEGER;
+UPDATE roads_ways SET id = gid;
 
--- Creatating a Table to store Service area of each hospital
-DROP TABLE IF EXISTS distances;
-CREATE TABLE distances (
-    gid        INTEGER,
-    service_area     GEOMETRY,
-);
+\o service_area.txt
+SELECT id,source,target,agg_cost, r.the_geom 
+FROM pgr_drivingDistance(
+        'SELECT id,source,target, length_m/60 AS cost,length_m/60 AS reverse_cost 
+        FROM roads.roads_ways',
+        13, 10, FALSE
+      ), roads.roads_ways AS r
+WHERE edge = r.gid;
 
--- Calculating and storing Service area of each hospital
-UPDATE distances 
-SET service_area = service_area(tag_id)::GEOMETRY; 
+\o correct_service_area.txt
+WITH subquery AS (
+SELECT r.gid, edge,source,target,agg_cost, r.population,r.the_geom 
+FROM pgr_drivingDistance(
+        'SELECT id,source,target, length_m/60 AS cost, length_m/60 AS reverse_cost 
+        FROM roads.roads_ways',
+        (SELECT closest_vertex(poly_geom) 
+        FROM buildings.buildings_ways 
+        WHERE tag_id = '318'
+        ), 10, FALSE
+      ),roads.roads_ways AS r
+WHERE edge = r.gid)
+SELECT r.id, r.the_geom 
+FROM subquery AS s,roads.roads_ways AS r 
+WHERE r.source = s.source OR r.target = s.target;
 
--- Finding the areas which are not covered by current hospitals
-
-	-- 1. Finding Polygons of the service area of the current hospitals
-	-- 2. Finding intersection of the buildings and polygons with the service area
-
-
--- Proposing new locations for mobile hospitals
-
-	-- 1. Checking availability of schools/universities/hotels/grounds for temporary hospital locations
-	-- 2. If present, assigning the optimal locations to them.
-	-- 3. Check the service area once again. If the service area cover the whole study area, Task is completed.
-	
-	-- Questions: 
-		-- i. What if no schools/universities/hotels/grounds are available?
-		-- ii. What is the service area of new polygon doesnot satisfy the needs of complete area?
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+\o total_population.txt
+-- finding total population
+WITH subquery
+AS (
+	WITH subquery AS (
+	SELECT r.gid,source,target,agg_cost, r.population,r.the_geom 
+	FROM pgr_drivingDistance(
+		    'SELECT id,source,target, length_m/60 AS cost, length_m/60 AS reverse_cost 
+		    FROM roads.roads_ways',
+		    (SELECT closest_vertex(poly_geom) 
+		    FROM buildings.buildings_ways 
+		    WHERE tag_id = '318'
+		    ), 10, FALSE
+		  ),roads.roads_ways AS r
+	WHERE edge = r.gid)
+	SELECT r.id, r.the_geom, s.population
+	FROM subquery AS s,roads.roads_ways AS r 
+	WHERE r.source = s.source OR r.target = s.target
+	)
+SELECT sum(population) FROM subquery;
 \o
