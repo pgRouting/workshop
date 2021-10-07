25fcee4d4c3c7e715c51542e89ccec4393dee383
Vicky Vergara <vicky@georepublic.de>
2021-10-07 10:21:07 -0500

    [build] create bangladesh database

--- docs/scripts/un_sdg/sdg3/get_mumbai_data.sh
+++ docs/scripts/un_sdg/sdg3/get_mumbai_data.sh
@@ -4,6 +4,6 @@ set -e
 # mumbai data from-here
 CITY="mumbai"
 wget -N --progress=dot:mega \
-"https://download.osgeo.org/pgrouting/workshops/mumbai.osm.bz2"
+"https://download.osgeo.org/pgrouting/workshops/$CITY.osm.bz2"
 bunzip2 -f "$CITY.osm.bz2"
 # mumbai data to-here
