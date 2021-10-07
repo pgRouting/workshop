cb20d9bfab96e5418e5a189848dfdea8056b7e39
Vicky Vergara <vicky@georepublic.de>
2021-10-07 10:16:14 -0500

    [build] get Bangladesh data snapshopt

--- docs/scripts/un_sdg/sdg11/get_bangladesh.sh
+++ docs/scripts/un_sdg/sdg11/get_bangladesh.sh
@@ -5,4 +5,7 @@ set -e
 CITY="bangladesh"
 BBOX="88.9515,22.2192,89.3806,22.4310"
 wget --progress=dot:mega -O "$CITY.osm" "http://www.overpass-api.de/api/xapi?*[bbox=${BBOX}][@meta]"
+
+osmconvert --drop-author --drop-version bangladesh.osm -o=bangladesh_pass1.osm
+osmfilter bangladesh_pass1.osm -o=bangladesh.osm --drop="highway= building="
 # get_bangladesh to-here
