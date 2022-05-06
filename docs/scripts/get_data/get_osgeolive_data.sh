d6b8b9ea58be12073da2638ab4a84c3fa23d7001
cvvergara <vicky@georepublic.de>
2022-05-06 12:09:58 -0500

    [data] getting data from osgeolive backup location

--- docs/scripts/get_data/get_osgeolive_data.sh
+++ docs/scripts/get_data/get_osgeolive_data.sh
@@ -2,9 +2,10 @@
 set -e
 
 # 4.2.2 from-here
+# TODO Use the /15/osm  instead of data/osm/FIRENZE_IT
 CITY="@PGR_WORKSHOP_CITY_FILE@"
 wget -N --progress=dot:mega \
-"http://download.osgeo.org/livedvd/14/osm/$CITY.osm.bz2"
+"http://download.osgeo.org/livedvd/data/osm/FIRENZE_IT/$CITY.osm.bz2"
 bunzip2 -f "$CITY.osm.bz2"
 # 4.2.2 to-here
 
