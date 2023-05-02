c8417d67c9d4e9281d01f64b4a17152a14f3cbf5
cvvergara <vicky@georepublic.de>
2023-05-02 12:20:46 -0600

    [pedestrian] refining pedestrian chapter

--- docs/scripts/basic/chapter_5/images.sql
+++ docs/scripts/basic/chapter_5/images.sql
@@ -69,8 +69,8 @@ SELECT * FROM pgr_dijkstra(
 SELECT seq, start_vid, end_vid, the_geom AS geom FROM dijkstra JOIN ways ON(edge = gid);
 
 
-/*
-CREATE VIEW pedestrian_route4 AS
+CREATE VIEW pedestrian_route5 AS
+WITH dijkstra AS (
 SELECT *
 FROM pgr_dijkstraCost(
     '
@@ -84,5 +84,7 @@ FROM pgr_dijkstraCost(
     ARRAY[@ID_4@, @ID_5@],
     directed := false)
 )
-SELECT seq, start_vid, end_vid, the_geom AS geom FROM dijkstra JOIN ways ON(edge = gid);
-*/
+SELECT row_number() over() AS gid, start_vid, end_vid, agg_cost, ST_MakeLine(v1.the_geom, v2.the_geom) AS geom FROM dijkstra
+JOIN ways_vertices_pgr AS v1 ON (start_vid = v1.id)
+JOIN ways_vertices_pgr AS v2 ON (end_vid = v2.id);
+
