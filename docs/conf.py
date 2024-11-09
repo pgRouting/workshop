9f93c2ede89448e0ffe947a5ee772437a4409341
cvvergara <vicky@erosion.dev>
2024-11-09 13:20:55 -0600

    (build,CI) adjusting the requirements and build options

--- docs/conf.py
+++ docs/conf.py
@@ -181,7 +181,9 @@ html_last_updated_fmt = '%b %d, %Y'
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {
+    '**': []
+    }
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
