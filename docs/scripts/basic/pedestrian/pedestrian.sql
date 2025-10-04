67d740f2036fb54940d916e4f84d4508a544fe93
cvvergara <vicky@erosion.dev>
2025-10-04 11:09:48 -0600

    (basic/pedestrian) scripts using the walk net for pedestrian routing

--- docs/scripts/basic/pedestrian/pedestrian.sql
+++ docs/scripts/basic/pedestrian/pedestrian.sql
@@ -1,111 +1,70 @@
-\o exercise_5_0.txt
+\o get_id.txt
 
-SELECT osm_id, id FROM ways_vertices_pgr
+SELECT osm_id, id FROM vertices
 WHERE osm_id IN (@OSMID_1@, @OSMID_2@, @OSMID_3@, @OSMID_4@, @OSMID_5@)
 ORDER BY osm_id;
 
-\o exercise_5_1.txt
+\o one_to_one.txt
 
 SELECT * FROM pgr_dijkstra(
-  '
-    SELECT gid AS id,
-      source,
-      target,
-      length AS cost      -- line 6
-    FROM ways
-  ',
-  @ID_1@, -- line 9
-  @ID_3@, -- line 10
-  directed := false); -- line 11
-
-\o exercise_5_2.txt
-
-SELECT * FROM pgr_dijkstra(
-  '
-    SELECT gid AS id,
-      source,
-      target,
-      length_m AS cost      -- line 6
-    FROM ways
-  ',
-  ARRAY[@ID_1@,@ID_2@], -- line 9
-  @ID_3@, -- line 10
+  'SELECT id, source, target,
+    length AS cost
+    FROM walk_net',
+  @ID_1@,
+  @ID_3@,
   directed := false);
 
-\o exercise_5_3.txt
+\o many_to_one.txt
 
 SELECT * FROM pgr_dijkstra(
-  '
-    SELECT gid AS id,
-      source,
-      target,
-      length_m / 1.3 AS cost      -- line 6
-    FROM ways
-  ',
-  @ID_3@, -- line 9
-  ARRAY[@ID_1@,@ID_2@], -- line 10
+  'SELECT id, source, target,
+    length/1000 AS cost
+    FROM walk_net',
+  ARRAY[@ID_1@, @ID_2@],
+  @ID_3@,
   directed := false);
 
-\o exercise_5_4.txt
+\o one_to_many.txt
 
 SELECT * FROM pgr_dijkstra(
-  '
-    SELECT gid AS id,
-      source,
-      target,
-      length_m / 1.3 / 60 AS cost -- line 6
-    FROM ways
-  ',
-  ARRAY[@ID_1@, @ID_2@], -- line 9
-  ARRAY[@ID_4@, @ID_5@], -- line 10
+  'SELECT id, source, target,
+    cost
+    FROM walk_net',
+  @ID_3@,
+  ARRAY[@ID_1@, @ID_2@],
   directed := false);
 
-\o exercise_5_5.txt
+\o many_to_many.txt
 
 SELECT * FROM pgr_dijkstra(
-  '
-    SELECT gid AS id,
-      source,
-      target,
-      length_m / 1.3 / 60 AS cost
-    FROM ways
-  ',
-  'SELECT * FROM
-    (VALUES
-      (@ID_1@, @ID_4@), -- line 11
-      (@ID_2@, @ID_5@)) -- line 12
-    AS combinations (source, target)',
+  'SELECT id, source, target,
+    length / 1.3 / 60 AS cost
+    FROM walk_net',
+  ARRAY[@ID_1@, @ID_2@],
+  ARRAY[@ID_4@, @ID_5@],
   directed := false);
 
-\o exercise_5_6.txt
+\o combinations.txt
+
+SELECT * FROM pgr_dijkstra(
+  'SELECT id, source, target,
+    length / 1.3 / 60 AS cost
+    FROM walk_net',
+  'SELECT * FROM (VALUES
+    (@ID_1@, @ID_4@),
+    (@ID_2@, @ID_5@))
+  AS combinations (source, target)',
+  directed := false);
+
+\o dijkstracost.txt
 
 SELECT * FROM pgr_dijkstraCost(
-  '
-    SELECT gid AS id,
-      source,
-      target,
-      length_m / 1.3 / 60 AS cost    -- line 6
-    FROM ways
-  ',
-  ARRAY[@ID_1@, @ID_2@], -- line 9
-  ARRAY[@ID_4@, @ID_5@], -- line 10
+  'SELECT id, source, target,
+    length / 1.3 / 60 AS cost
+    FROM walk_net',
+  ARRAY[@ID_1@, @ID_2@],
+  ARRAY[@ID_4@, @ID_5@],
   directed := false);
-
-\o exercise_5_7.txt
-
-SELECT start_vid, sum(agg_cost) FROM pgr_dijkstraCost(
-  '
-    SELECT gid AS id,
-      source,
-      target,
-      length_m / 1.3 / 60 AS cost -- line 6
-    FROM ways
-  ',
-  ARRAY[@ID_1@, @ID_2@], -- line 9
-  ARRAY[@ID_4@, @ID_5@], -- line 10
-  directed := false)
-GROUP BY start_vid
-ORDER BY start_vid;
 
 \o note_1.txt
 \t
@@ -114,11 +73,11 @@ SELECT 'Inspecting the results, looking for totals (edge = -1):';
 SELECT '- From @ID_1@ to vertex @ID_4@ takes ' || round(agg_cost::numeric, 2) || ' minutes (seq = ' || seq || ')'
 FROM pgr_dijkstra(
   '
-  SELECT gid AS id,
+  SELECT id,
   source,
   target,
-  length_m / 1.3 / 60 AS cost
-  FROM ways
+  length / 1.3 / 60 AS cost
+  FROM walk_net
   ',
   ARRAY[@ID_1@, @ID_2@],
   ARRAY[@ID_4@, @ID_5@],
@@ -127,11 +86,11 @@ WHERE edge = -1 AND start_vid = @ID_1@ AND end_vid = @ID_4@;
 SELECT '- From @ID_1@ to vertex @ID_5@ takes ' || round(agg_cost::numeric, 2) || ' minutes (seq = ' || seq || ')'
 FROM pgr_dijkstra(
   '
-  SELECT gid AS id,
+  SELECT id,
   source,
   target,
-  length_m / 1.3 / 60 AS cost
-  FROM ways
+  length / 1.3 / 60 AS cost
+  FROM walk_net
   ',
   ARRAY[@ID_1@, @ID_2@],
   ARRAY[@ID_4@, @ID_5@],
@@ -140,11 +99,11 @@ WHERE edge = -1 AND start_vid = @ID_1@ AND end_vid = @ID_5@;
 SELECT '- From @ID_2@ to vertex @ID_4@ takes ' || round(agg_cost::numeric, 2) || ' minutes (seq = ' || seq || ')'
 FROM pgr_dijkstra(
   '
-  SELECT gid AS id,
+  SELECT id,
   source,
   target,
-  length_m / 1.3 / 60 AS cost
-  FROM ways
+  length / 1.3 / 60 AS cost
+  FROM walk_net
   ',
   ARRAY[@ID_1@, @ID_2@],
   ARRAY[@ID_4@, @ID_5@],
@@ -153,29 +112,13 @@ WHERE edge = -1 AND start_vid = @ID_2@ AND end_vid = @ID_4@;
 SELECT '- From @ID_2@ to vertex @ID_5@ takes ' || round(agg_cost::numeric, 2) || ' minutes (seq = ' || seq || ')'
 FROM pgr_dijkstra(
   '
-  SELECT gid AS id,
+  SELECT id,
   source,
   target,
-  length_m / 1.3 / 60 AS cost
-  FROM ways
+  length / 1.3 / 60 AS cost
+  FROM walk_net
   ',
   ARRAY[@ID_1@, @ID_2@],
   ARRAY[@ID_4@, @ID_5@],
   directed := false)
 WHERE edge = -1 AND start_vid = @ID_2@ AND end_vid = @ID_5@;
-\o note_2.txt
-WITH summary AS (SELECT start_vid, sum(agg_cost)
-  FROM pgr_dijkstraCost(
-    'SELECT gid AS id, source, target, length_m  / 1.3 / 60 AS cost FROM ways ',
-    ARRAY[@ID_1@, @ID_2@], ARRAY[@ID_4@, @ID_5@],
-    directed := false)
-  GROUP BY start_vid
-  ORDER BY start_vid),
-the_min AS (select min(sum) from summary)
-SELECT
-'An interpretation of the result can be: In general, it is faster to depart from "' ||
-CASE WHEN start_vid = @ID_1@ THEN '@PLACE_1@' WHEN start_vid = @ID_2@ THEN '@PLACE_2@' END
-  || '" than from "' ||
-  CASE WHEN start_vid = @ID_1@ THEN '@PLACE_2@' WHEN start_vid = @ID_2@ THEN '@PLACE_1@' END
-    || '"'
-    FROM summary WHERE sum = (SELECT min from the_min);
